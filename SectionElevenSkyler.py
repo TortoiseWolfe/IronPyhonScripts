a56732a544bcb3260fb4236cbe482caa49997482
Turtle Wolfe <JonPohlner@gmail.com>
2022-06-10 11:34:05 -0400

    before Skyler

--- SectionElevenSkyler.py
+++ SectionElevenSkyler.py
@@ -9,6 +9,7 @@ import RevitServices
 import Revit
 import Autodesk
 from Autodesk.Revit.DB import *
+import math
 from math import *
 
 clr.ImportExtensions(Revit.GeometryConversion)
@@ -19,7 +20,7 @@ from RevitServices.Transactions import TransactionManager
 doc = DocumentManager.Instance.CurrentDBDocument
 
 toggle = IN[0]
-points = UnwrapElement(IN[1])
+point = UnwrapElement(IN[1])
 modelPoints = UnwrapElement(IN[2])
 cropCurves = UnwrapElement(IN[3])
 viewType = UnwrapElement(IN[4])
@@ -40,62 +41,94 @@ if toggle == True:
 	
 	TransactionManager.Instance.EnsureInTransaction(doc)
 	
-	for ind, point in enumerate(points):
+
 	
-		#Retrieve the mid point of model lines and get X,Y.		
-		modelMP = modelPoints[ind].ToXyz()
+		#Retrieve the mid point of model lines and get X,Y.	
+	index = 0
+	for modelpoint in modelPoints:	
+		modelMP = modelpoint.ToXyz()
 		modelMPX = modelMP.X
 		modelMPY = modelMP.Y
-		
-		#Retrieve individual lines of crop window.		
-		cropLines = cropCurves[ind]
+			
+			#Retrieve individual lines of crop window.		
+		cropLines = cropCurves[index]
 		l1 = cropLines[0].ToRevitType()
 		l2 = cropLines[1].ToRevitType()
 		l3 = cropLines[2].ToRevitType()
 		l4 = cropLines[3].ToRevitType()
-					
-		# Create a line in the z-Axis for elevation marker to rotate around.			
-		elevationPT = point.ToXyz()
+						
+			# Create a line in the z-Axis for elevation marker to rotate around.			
+		elevationPT = point[index].ToXyz()
 		elptRotate = XYZ(elevationPT.X, elevationPT.Y, elevationPT.Z+100)
 		ln = Line.CreateBound(elevationPT, elptRotate)
-
-		#Calculate the angle between Model Mid Point and Elevation Point.
+	
+			#Calculate the angle between Model Mid Point and Elevation Point.
 		elevationPTY = elevationPT.Y
-		elevationPTX = elevationPT.X							
-		combY = elevationPTY-modelMPY
-		combX = elevationPTX-modelMPX			
-		ang = atan2(combY, combX)
-
-		#Create elevation marker and elevation in position 0.
+		elevationPTX = elevationPT.X	
+		
+		
+		#combY = elevationPTY-modelMPY
+		#combX = elevationPTX-modelMPX	
+		
+		combY = modelMPY-elevationPTY
+		combX = modelMPX-elevationPTX			
+		ang = -atan2(combX, combY)
+		
+		comparisonAng = round(degrees(ang))
+		newAng = 90
+		arrowPos = 2
 		eleMarker = ElevationMarker.CreateElevationMarker(doc, viewType.Id, elevationPT, 100)
-		ele = eleMarker.CreateElevation(doc, doc.ActiveView.Id , 0)
-		
-		#Rotate elevation marker towars model line.
+		#left arrow
+		if comparisonAng >= 45 and comparisonAng <= 135:
+			arrowPos = 0
+			newAng = -90
+		#top arrow
+		#changed <= to < august 5th 2021
+		if comparisonAng <= 45 and comparisonAng > -45:
+			arrowPos = 1
+			newAng = 0
+		#bottom arrow
+		if comparisonAng > 135 or comparisonAng < -135:
+			arrowPos = 3
+			ang = ang + radians(90)
+			
+		#default is right arrow
+		ele = eleMarker.CreateElevation(doc, doc.ActiveView.Id , arrowPos)
 		ElementTransformUtils.RotateElement(doc, eleMarker.Id, ln, ang)
+		ElementTransformUtils.RotateElement(doc, eleMarker.Id, ln, radians(newAng))
 		
-		#	
+		#ElementTransformUtils.RotateElement(doc, eleMarker.Id, ln, ang)
+			#Create elevation marker and elevation in position 0.
+			
+			#Rotate elevation marker towars model line.
+		
+		#ElementTransformUtils.RotateElement(doc, eleMarker.Id, ln, math.radians(90))
+			#	
 		crManager = ele.GetCropRegionShapeManager()
-		#crShape = crManager.GetCropRegionShape()
-
+			#crShape = crManager.GetCropRegionShape()
+	
 		newCurveLoop = []
 		newCurveLoop.append(l1)
 		newCurveLoop.append(l2)
 		newCurveLoop.append(l3)
 		newCurveLoop.append(l4)
-			
+				
 		cLoop = CurveLoop.Create(newCurveLoop)
+		index+=1
 
 		try:			
-			crManager.SetCropRegionShape(cLoop)
-			lst.append("Elevation Created")
-		
+			crManager.SetCropShape(cLoop)
+			lst.append(ele)
+			#lst.append("Elevation Created")
+			
 		except:
 			pass
 			lst.append("Missed Elevation")
-
-	TransactionManager.Instance.TransactionTaskDone()
 	
-	OUT = lst
+		TransactionManager.Instance.TransactionTaskDone()
+		
+		#OUT = degrees(ang)
+		OUT = lst
 	
 else:
 
